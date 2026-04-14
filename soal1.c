0d3daa96ca1160b8cb790724094e482f346d1899
HasanSan-png <hasan.mutawakkil.al@gmail.com>
2026-04-14 08:53:28 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -5,7 +5,7 @@ int main (){
     printf("masukkan nilai n :");
     scanf("%d", &N);
 
-    for (int i = 1; i < N; i++)
+    for (int i = 0; i <= N; i++)
     {
             if (i%2 == 0 && i%3 == 0)
             {
